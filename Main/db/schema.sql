fa7aaaf8f0040c2a0ac224d1ffc809cff1f48272
cjclap <dmxtechinn@gmail.com>
2022-11-01 02:33:00 -0600

    correcting  VARCHAR

--- Main/db/schema.sql
+++ Main/db/schema.sql
@@ -3,18 +3,18 @@ CREATE DATABASE employee_db;
 USE employee_db;
 CREATE TABLE employees (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(20) NOT NULL,
-    last_name VARCHAR(20) NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
     role_id INT,
     manager_id INT,
 );
 CREATE TABLE roles (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    title VARCHAR(20) NOT NULL,
+    title VARCHAR(30) NOT NULL,
     salary DECIMAL NOT NULL,
     department_id INT,
 );
 CREATE TABLE departments (
     id INT PRIMARY KEY AUTO_INCREMENT,
-    name VARCHAR(20) NOT NULL
+    name VARCHAR(30) NOT NULL
 );
