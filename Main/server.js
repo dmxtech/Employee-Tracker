ba471ca93886cd8037e816b37cb5750517fc3514
cjclap <dmxtechinn@gmail.com>
2022-10-31 22:56:25 -0600

    starting to add inquirer prompt and installed all dependencies

--- Main/server.js
+++ Main/server.js
@@ -2,3 +2,23 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const consTable = require('console.table');
 require('dotenv').config()
+
+
+function promptUser() {
+    inquirer.prompt({
+        type: 'list',
+        message: 'What would you like to do?',
+        choices: [
+            'View All Employees',
+            'Add Employee',
+            'Update Employee Role',
+            'View All Roles',
+            'Add Role',
+            'View All Departments',
+            'Add Departments',
+            'Quit'
+        ],
+        name: 'indexmenu',
+    })
+        .then(answers => { }
+}
