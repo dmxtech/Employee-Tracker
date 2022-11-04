3a5b9a14bc320efe00117b4c473e7dfe7e44c237
cjclap <dmxtechinn@gmail.com>
2022-11-04 00:32:16 -0600

    updated view functions and working on details

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ const db = mysql.createConnection(
 );
 db.connect(err => {
     if (err) throw err;
-
+    Welcomemessage()
 });
 function Welcomemessage() {
     console.log("._________________________________.")
@@ -94,73 +94,123 @@ function Welcomemessage() {
 
 
             function AddEmployee() {
+
+
                 let managerArray = [{
                     name: 'None',
                     value: 'NULL'
                 }];
 
                 let roleArray = [];
-                return inquirer.prompt(
-                    [
-                        {
-                            type: 'input',
-                            message: "What is the employee's first name?",
-                            name: 'firstName',
-                        },
-                        {
-                            type: 'input',
-                            message: "What is the employee's last name?",
-                            name: 'lastName',
-                        },
-                        {
-                            type: 'list',
-                            message: "What is the employee's role?",
-                            choices: roleArray,
-                            name: 'employeeRole'
-                        },
-                        {
-                            type: 'list',
-                            message: "Who is the employee's manager?",
-                            choices: managerArray,
-                            name: 'employeeManager'
+
+                db.query("SELECT * FROM employees WHERE manager_id IS NULL", function (err, results) {
+                    if (err) throw err;
+
+                    for (let i = 0; i < results.length; i++) {
+                        managerArray.push({
+                            name: results[i].first_name + ' ' + results[i].last_name,
+                            value: results[i].id
+                        });
+                    }
+
+                    db.query("SELECT * FROM roles", function (err, results) {
+                        if (err) throw err;
+                        for (let i = 0; i < results.length; i++) {
+                            roleArray.push({
+                                name: results[i].title,
+                                value: results[i].id
+                            });
                         }
-                    ]
-                ).then(answers => {
-                    const sqlQuery = `
+                    })
+                    return inquirer.prompt(
+                        [
+                            {
+                                type: 'input',
+                                message: "What is the employee's first name?",
+                                name: 'firstName',
+                            },
+                            {
+                                type: 'input',
+                                message: "What is the employee's last name?",
+                                name: 'lastName',
+                            },
+                            {
+                                type: 'list',
+                                message: "What is the employee's role?",
+                                choices: roleArray,
+                                name: 'employeeRole'
+                            },
+                            {
+                                type: 'list',
+                                message: "Who is the employee's manager?",
+                                choices: managerArray,
+                                name: 'employeeManager'
+                            }
+                        ]
+                    )
+                        .then(answers => {
+                            const sqlQuery = `
                     INSERT INTO employees (first_name, last_name, role_id, manager_id) 
                     VALUES 
                     ('${answers.firstName}', '${answers.lastName}', ${answers.employeeRole}, ${answers.employeeManager})`;
 
-                    db.query(sqlQuery, function (err, results) {
-                        if (err) throw err;
-                        console.log('New employee added.');
-                        Welcomemessage();
-                    });
+                            db.query(sqlQuery, function (err, results) {
+                                if (err) throw err;
+                                console.log('New employee added');
+                                Welcomemessage();
+                            });
+                        })
                 })
+
             };
-
-
 
             function UpdateEmployeeRole() {
                 const employeeArray = [];
                 const roleArray = [];
-                return inquirer.prompt(
-                    [
-                        {
-                            type: 'list',
-                            message: "Which employee's role do you want to update?",
-                            choices: employeeArray,
-                            name: 'employeeUpdate'
+                db.query("SELECT * FROM employees", function (err, results) {
+                    if (err) throw err;
+                    for (let i = 0; i < results.length; i++) {
+                        employeeArray.push({
+                            name: results[i].first_name + ' ' + results[i].last_name,
+                            value: results[i].id
+                        });
+                    }
 
-                        },
-                        {
-                            type: 'list',
-                            message: 'Which role do you want to assign the selected employee?',
-                            choices: roleArray,
-                            name: 'newRole'
+                    db.query("SELECT * FROM roles", function (err, results) {
+                        if (err) throw err;
+
+                        for (let i = 0; i < results.length; i++) {
+                            roleArray.push({
+                                name: results[i].title,
+                                value: results[i].id
+                            });
                         }
-                    ]
-                )
+                        return inquirer.prompt(
+                            [
+                                {
+                                    type: 'list',
+                                    message: "Which employee's role do you want to update?",
+                                    choices: employeeArray,
+                                    name: 'employeeUpdate'
+
+                                },
+                                {
+                                    type: 'list',
+                                    message: 'Which role do you want to assign the selected employee?',
+                                    choices: roleArray,
+                                    name: 'newRole'
+                                }
+                            ]
+                        ).then(answers => {
+                            db.query(`UPDATE employees SET role_id = ${answers.newRole} WHERE id = ${answers.employeeUpdate}`, function (err, results) {
+                                if (err) throw err;
+                                console.table('Employee has been updated.');
+                                promptUser();
+                            });
+                        })
+                    })
+                })
+
             };
 
 
@@ -183,33 +233,43 @@ function Welcomemessage() {
 
             function AddRole() {
                 let departmentArray = [];
-                return inquirer.prompt(
-                    [
-                        {
-                            type: 'input',
-                            message: 'What is the name of the role?',
-                            name: 'roleName'
-                        },
-                        {
-                            type: 'input',
-                            message: 'What is the salary of the role?',
-                            name: 'roleSalary'
-                        },
-                        {
-                            type: 'list',
-                            message: 'Which department does the role belong to?',
-                            choices: departmentArray,
-                            name: 'roleDepartment'
+                db.query("SELECT * FROM departments", function (err, results) {
+                    if (err) throw err;
+                    for (let i = 0; i < results.length; i++) {
+                        let depto = {
+                            name: results[i].name,
+                            value: results[i].id
                         }
-                    ]
-                )
-                    .then(answers => {
-                        db.query(`INSERT INTO roles (title, salary, department_id) VALUES ('${answers.roleName}', ${answers.roleSalary}, ${answers.roleDepartment})`, function (err, results) {
-                            if (err) throw err;
-                            console.log('New role added.');
-                            Welcomemessage();
-                        });
-                    })
+                        departmentArray.push(depto);
+                    }
+                    return inquirer.prompt(
+                        [
+                            {
+                                type: 'input',
+                                message: 'What is the name of the role?',
+                                name: 'roleName'
+                            },
+                            {
+                                type: 'input',
+                                message: 'What is the salary of the role?',
+                                name: 'roleSalary'
+                            },
+                            {
+                                type: 'list',
+                                message: 'Which department does the role belong to?',
+                                choices: departmentArray,
+                                name: 'roleDepartment'
+                            }
+                        ]
+                    )
+                        .then(answers => {
+                            db.query(`INSERT INTO roles (title, salary, department_id) VALUES ('${answers.roleName}', ${answers.roleSalary}, ${answers.roleDepartment})`, function (err, results) {
+                                if (err) throw err;
+                                console.log('New role added.');
+                                Welcomemessage();
+                            });
+                        })
+                })
             };
 
             function ViewallDepartments() {
@@ -248,6 +308,7 @@ function Welcomemessage() {
 
 
         })
+
 
 }
 Welcomemessage()
